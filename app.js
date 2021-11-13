f64e943c4aa91772f92896295ba4a4fa5a7f7b80
Suraez <oberais50@gmail.com>
2021-11-14 00:49:30 +0545

    developed api for search

---
+++ app.js
@@ -0,0 +1,31 @@
+const { default: axios } = require('axios');
+const express = require('express');
+const app = express()
+
+const PORT = process.env.PORT || 4000;
+
+
+app.get('/search/repositories', (req, res) => {
+
+    axios.get('https://api.github.com/search/repositories?q=' + req.query.q)
+        .then(response => {
+            res.status(200).send({
+                msg: "success",
+                data: response.data
+            });
+            console.log(res);  
+        })
+        .catch(err => {
+            res.send('oops! error occurred!');
+            console.log(err);
+        })
+
+})
+
+app.get('/', (req, res) => {
+    res.send('hello world!');
+})
+
+app.listen(PORT, () => {
+    console.log(`listening on PORT ${PORT}`);
+})
